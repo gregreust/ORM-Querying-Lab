9083af08a4a1ab7a46a6aba0b922d3d6beb71ed7
gregreust <greg.reust@gmail.com>
2022-08-15 14:21:07 -0500

    Problem 5 complete

--- school_db/views.py
+++ school_db/views.py
@@ -247,8 +247,12 @@ SELECT COUNT(*) AS `__count`
 # Print the new student's id, full name, year, and gpa to the terminal
 # NOTE every time you execute this function a duplicate student will be created with a different primary key number
 def problem_five(request):
-
-    return complete(request)
+  new_student = Student.objects.create(first_name = 'Greg', last_name = 'Reust', year = 2013, gpa = 3.8)
+  print(f'Id: {new_student.id}')
+  print(f'Full Name: {new_student.first_name} {new_student.last_name}')
+  print(f'Year: {new_student.year}')
+  print(f'GPA: {new_student.gpa}')
+  return complete(request)
 
 
 # Supporting Query Method Documentation:
